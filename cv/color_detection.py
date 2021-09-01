1ada42b445da867ae0e9237ecb25caef45609c81
doron <doron@vfunction.com>
2021-09-01 16:58:56 +0300

    add body color, and automate paint to cv hue conversion

--- cv/color_detection.py
+++ cv/color_detection.py
@@ -19,17 +19,21 @@ if __name__ == '__main__':
     hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
 
     # define the list of boundaries
-    colors = [[231 * 179 / 239, 185, 174],  # pink
-              [25 * 179 / 239, 223, 160],  # yellow
-              [137 * 179 / 239, 137, 185],  # light-blue
-              [77 * 179 / 239, 84, 78]  # light-blue
+    colors = [[231, 185, 174],  # pink
+              [25, 223, 160],  # yellow
+              [137, 137, 185],  # light-blue
+              [77, 84, 78],  # light-blue
+              [5, 132, 204]  # body
               ]
-    colors = [np.array(c, dtype="uint8") for c in colors]
-    print(colors)
+
+    # transform from paint to openCV hue scale
+    r = 179 / 239
+    colors = np.array([[int(a * b) for a, b in zip(color, [r, 1, 1])] for color in colors],  dtype="uint8")
+
     d = np.array([22, 100, 150], dtype="uint8")
     max_values = np.array([179, 255, 255], dtype="uint8")
     boundaries = [(safe_subtract(color, d), safe_add(color, d, max_values)) for color in colors]
-    print(boundaries)
+
     # loop over the boundaries
     for (lower, upper) in boundaries:
         # find the colors within the specified boundaries and apply
